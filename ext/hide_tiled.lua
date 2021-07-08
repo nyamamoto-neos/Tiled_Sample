127f8fdfc8f776d549085bb0c7f1a15f9a324ac6
Naoya Yamamoto <86646243+nyamamoto-neos@users.noreply.github.com>
2021-07-08 10:32:22 +0900

    Update hide_tiled.lua

--- ext/hide_tiled.lua
+++ ext/hide_tiled.lua
@@ -2,3 +2,6 @@ print("hide")
 --- remove the listeners
 Runtime:removeEventListener( "enterFrame", UI.enterFrame )
 Runtime:removeEventListener( "key", UI.key )
+
+--- remove the map
+layer.map:removeSelf()
